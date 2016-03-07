dff8214e3972f3d8707f723715fe2f07d0243584
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-03-07 18:27:41 +0000

    Rounding.

--- app/services/video/VideoService.scala
+++ app/services/video/VideoService.scala
@@ -166,10 +166,10 @@ object VideoService extends MediainfoInterpreter {
   private def padding(sourceDimensions: Option[(Int, Int)], outputSize: Option[(Int, Int)], rotationToApply: Int): Option[String] = {
     outputSize.flatMap { os =>
       sourceDimensions.flatMap { sd =>
-        val sourceAspectRatio = BigDecimal(sd._1) / BigDecimal(sd._2).setScale(1, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+        val sourceAspectRatio = (BigDecimal(sd._1) / BigDecimal(sd._2)).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
         Logger.info("Source dimensions " + sd + " aspect ratio: " + sourceAspectRatio)
 
-        val outputAspectRatio = BigDecimal(os._1) / BigDecimal(os._2).setScale(1, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+        val outputAspectRatio = (BigDecimal(os._1) / BigDecimal(os._2)).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
         Logger.info("Ouptut dimensions " + os + " aspect ratio: " + outputAspectRatio)
 
         val aspectRatiosDiffer: Boolean = sourceAspectRatio != outputAspectRatio
