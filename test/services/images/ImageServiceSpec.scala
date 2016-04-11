5c6c2f73166d83c60ef33a875db758e2e7c6b28a
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-04-11 11:40:55 +0100

    Test running in

--- test/services/images/ImageServiceSpec.scala
+++ test/services/images/ImageServiceSpec.scala
@@ -3,21 +3,26 @@ package services.images
 import java.io.File
 
 import org.specs2.mutable._
+import play.api.test.Helpers._
+import play.api.test._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class ImageServiceSpec extends Specification {
 
+  val port: Port = 3334
   val tenSeconds = Duration(10, SECONDS)
 
   "can determine the dimensions of an image" in {
-    val landscapeImageFile = new File("test/resources/IMG_9758.JPG")
+    running(TestServer(port)) {
+      val landscapeImageFile = new File("test/resources/IMG_9758.JPG")
 
-    val dimensions: (Int, Int) = Await.result(ImageService.info(landscapeImageFile), tenSeconds)
+      val dimensions: (Int, Int) = Await.result(ImageService.info(landscapeImageFile), tenSeconds)
 
-    dimensions._1 must equalTo(3456)
-    dimensions._2 must equalTo(2304)
+      dimensions._1 must equalTo(3456)
+      dimensions._2 must equalTo(2304)
+    }
   }
 
 }
