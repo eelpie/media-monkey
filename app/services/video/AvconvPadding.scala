cc8c3675b472f18559cd97ccce163dfb2b987ae1
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-07-31 10:50:31 +0100

    Forcing an edge case which acvonv rounds incorrectly for small thumbnail.
    [pad @ 0x1091560] Input area 0:0:854:480 not within the padded area 0:0:852:480 or zero-sized

--- app/services/video/AvconvPadding.scala
+++ app/services/video/AvconvPadding.scala
@@ -4,13 +4,15 @@ import play.api.Logger
 
 trait AvconvPadding {
 
+  val SixteenNine = (BigDecimal(16) / BigDecimal(9)).setScale(10, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+
   def padding(sourceDimensions: Option[(Int, Int)], outputSize: Option[(Int, Int)], rotationToApply: Int): Option[String] = {
     outputSize.flatMap { os =>
       sourceDimensions.flatMap { sd =>
-        val sourceAspectRatio = (BigDecimal(sd._1) / BigDecimal(sd._2)).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+        val sourceAspectRatio = (BigDecimal(sd._1) / BigDecimal(sd._2)).setScale(10, BigDecimal.RoundingMode.HALF_DOWN).toDouble
         Logger.info("Source dimensions " + sd + " aspect ratio: " + sourceAspectRatio)
 
-        val outputAspectRatio = (BigDecimal(os._1) / BigDecimal(os._2)).setScale(2, BigDecimal.RoundingMode.HALF_DOWN).toDouble
+        val outputAspectRatio = (BigDecimal(os._1) / BigDecimal(os._2)).setScale(10, BigDecimal.RoundingMode.HALF_DOWN).toDouble
         Logger.info("Ouptut dimensions " + os + " aspect ratio: " + outputAspectRatio)
 
         val aspectRatiosDiffer: Boolean = sourceAspectRatio != outputAspectRatio
@@ -18,7 +20,11 @@ trait AvconvPadding {
 
         if (aspectRatiosDiffer || isRotated) {
           Logger.info("Applying padding")
-          Some("pad=ih*16/9:ih:(ow-iw)/2:(oh-ih)/2")
+
+          val paddedWidth: BigDecimal = (BigDecimal(sd._2) * SixteenNine).setScale(0, BigDecimal.RoundingMode.UP)
+          val paddingParameter = Some("pad=" + Seq(paddedWidth.rounded.toInt, sd._2, 0, 0).mkString(":"))
+          Logger.info("Generated padding parameter: " + paddingParameter)
+          paddingParameter
 
         } else {
           Logger.info("No padding required")
--- test/services/video/AvconvPaddingSpec.scala
+++ test/services/video/AvconvPaddingSpec.scala
@@ -5,14 +5,12 @@ import org.specs2.mutable._
 class AvconvPaddingSpec extends Specification with AvconvPadding {
 
   "can generate padding arguments from source and destination dimensions" in {
-
       val sourceDimensions = (854, 480)
       val targetDimensions = (296, 163)
 
       val paddingParameter: Option[String] = padding(Some(sourceDimensions), Some(targetDimensions), 0)
 
-      paddingParameter must equalTo(Some("pad=ih*16/9:ih:(ow-iw)/2:(oh-ih)/2"))
-
+      paddingParameter must equalTo(Some("pad=854:480:0:0"))
   }
 
 }
