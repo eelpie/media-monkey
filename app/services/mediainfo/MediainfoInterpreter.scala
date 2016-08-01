7b57f2baba5b421b2972db861d22a42c96aa4818
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-08-01 09:22:46 +0100

    Clean up.

--- app/services/mediainfo/MediainfoInterpreter.scala
+++ app/services/mediainfo/MediainfoInterpreter.scala
@@ -4,21 +4,26 @@ import model.Track
 
 trait MediainfoInterpreter {
 
+  val Rotation: String = "Rotation"
+  val Video: String = "Video"
+  val Width: String = "Width"
+  val Height: String = "Height"
+
   def videoDimensions(mediainfoTracks: Option[Seq[Track]]): Option[(Int, Int)] = {
 
     def parsePixels(i: String): Int = {
       i.stripSuffix(" pixels").replaceAll(" ", "").toInt
     }
 
-    mediainfoTracks.flatMap(mi => {
-      mi.find(t => t.trackType == "Video").headOption.flatMap{vt =>
-        vt.fields.get("Width").flatMap(w =>
-          vt.fields.get("Height").map(h =>
+    mediainfoTracks.flatMap{ mi =>
+      mi.find(t => t.trackType == Video).flatMap{ vt =>
+        vt.fields.get(Width).flatMap(w =>
+          vt.fields.get(Height).map(h =>
             (parsePixels(w), parsePixels(h))
           )
         )
       }
-    })
+    }
   }
 
   def inferRotation(mediainfoTracks: Option[Seq[Track]]): Int = {
@@ -27,7 +32,7 @@ trait MediainfoInterpreter {
       r.replaceAll("[^\\d]", "").toInt
     }
 
-    mediainfoTracks.flatMap(ts => ts.find(t => t.trackType == "Video").flatMap(i => i.fields.get("Rotation"))).fold(0)(mir => parseRotation(mir))
+    mediainfoTracks.flatMap(ts => ts.find(t => t.trackType == Video).flatMap(i => i.fields.get(Rotation))).fold(0)(mir => parseRotation(mir))
   }
 
 }
