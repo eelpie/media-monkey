792705d4befd7414cf9e8bf12cb99508e859740d
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-01-08 11:09:03 +0000

    Face detection spec.

--- test/services/facedetection/FaceDetectorSpec.scala
+++ test/services/facedetection/FaceDetectorSpec.scala
@@ -4,13 +4,15 @@ import java.io.{File, FileInputStream}
 
 import org.specs2.mutable.Specification
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 class FaceDetectorSpec extends Specification {
 
   "can detect faces" in {
-    val landscapeImageFile = new File("test/resources/face.jpg")
-    val detectedFaces = FaceDetector.detectFaces(new FileInputStream(landscapeImageFile))
-    println(detectedFaces)
-    1 must equalTo(1)
+    val imageWithSingleFace = new File("test/resources/5282722938_e0e2515624_o.jpg")
+    val detectedFaces = Await.result(FaceDetector.detectFaces(new FileInputStream(imageWithSingleFace)), Duration(10, SECONDS))
+    detectedFaces.size must equalTo(1)
   }
 
 }
