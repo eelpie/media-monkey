aaf6333a3a88fde16861647ae5c32ef5aaca98ee
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-03-21 16:01:11 +0000

    Face detection was actually running on the main pool?
    Service unresponsive.

--- test/services/facedetection/FaceDetectorSpec.scala
+++ test/services/facedetection/FaceDetectorSpec.scala
@@ -3,16 +3,22 @@ package services.facedetection
 import java.io.File
 
 import org.specs2.mutable.Specification
+import play.api.test.Helpers.running
+import play.api.test.{Port, TestServer}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class FaceDetectorSpec extends Specification {
 
+  val port: Port = 3334
+
   "can detect faces" in {
-    val imageWithSingleFace = new File("test/resources/5282722938_e0e2515624_o.jpg")
-    val detectedFaces = Await.result(FaceDetector.detectFaces(imageWithSingleFace), Duration(10, SECONDS))
-    detectedFaces.size must equalTo(1)
+    running(TestServer(port)) {
+      val imageWithSingleFace = new File("test/resources/5282722938_e0e2515624_o.jpg")
+      val detectedFaces = Await.result(FaceDetector.detectFaces(imageWithSingleFace), Duration(10, SECONDS))
+      detectedFaces.size must equalTo(1)
+    }
   }
 
 }
