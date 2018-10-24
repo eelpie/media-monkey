5b21b240f9328c14d05556ec57344147a2ff02ec
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-10-24 10:19:39 +0100

    Inject DI'ed service via fakeApplication.

--- test/services/images/ImageServiceSpec.scala
+++ test/services/images/ImageServiceSpec.scala
@@ -2,28 +2,25 @@ package services.images
 
 import java.io.File
 
-import javax.inject.Inject
-import org.specs2.mutable._
-import play.api.test.Helpers._
-import play.api.test._
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class ImageServiceSpec @Inject()(imageService: ImageService) extends Specification {
+class ImageServiceSpec extends PlaySpec with GuiceOneServerPerSuite {
 
-  val port: Port = 3334
   val tenSeconds = Duration(10, SECONDS)
 
+  val imageService: ImageService = fakeApplication().injector.instanceOf[ImageService]
+
   "can determine the dimensions of an image" in {
-    running(TestServer(port)) {
-      val landscapeImageFile = new File("test/resources/IMG_9758.JPG")
+    val landscapeImageFile = new File("test/resources/IMG_9758.JPG")
 
-      val dimensions: (Int, Int) = Await.result(imageService.info(landscapeImageFile), tenSeconds)
+    val dimensions: (Int, Int) = Await.result(imageService.info(landscapeImageFile), tenSeconds)
 
-      dimensions._1 must equalTo(3456)
-      dimensions._2 must equalTo(2304)
-    }
+    dimensions._1 must equal (3456)
+    dimensions._2 must equal (2304)
   }
 
 }
